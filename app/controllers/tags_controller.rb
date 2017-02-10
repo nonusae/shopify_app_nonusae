f0df2e3bcd5d06f55caaf1a9b19046eaff191582
Natthakit <nonusae@gmail.com>
2017-02-10 14:22:08 +0700

    finish test in development

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -18,8 +18,13 @@ class TagsController < ApplicationController
 	end
 
 	def get_translated_tag
-		@tag = Tag.where(:title => params[:tags].split(","))
-		puts params[:tags].split(",").class
+		t = params[:tags].split(",") # use this instead of where for correct order
+		@tag= []
+		t.each do |tt|
+			tag = Tag.find_by_title(tt)
+			@tag << tag
+		end
+		puts params[:tags].split(",")
 		respond_to do |format|
 			format.json { render :json => @tag }
 		end 		
