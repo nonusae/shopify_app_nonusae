702628cf07eefea99c95d4ebd658182119927153
Natthakit <nonusae@gmail.com>
2017-04-03 13:58:56 +0700

    asd 5

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -132,7 +132,7 @@ private
 
  def check_billing
     bill = ShopifyAPI::RecurringApplicationCharge.all
-    puts "Bill is " + bill.to_s
+    puts "Bill is " + bill.first.to_s
     unless bill.nil?
       @new_user = false
     else
