dd88488bfd774a0b83992ed59e7b21c4df2789a3
Natthakit <nonusae@gmail.com>
2017-04-06 16:43:43 +0700

    first implement automated theme modification

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -116,7 +116,11 @@ class Admin::DashboardController < ShopifyApp::AuthenticatedController
           current_theme_name = theme_j["name"]
         end
       end 
-      puts "Theme name is : " + current_theme_name ##   
+      puts "Theme name is : " + current_theme_name ##
+
+      if current_theme_id == "162888069"
+        file_to_overwrite = ShopifyAPI::Asset.create(key: 'section/collection-filters.liquid', src: 'https://rawgit.com/nonusae/shopify_app_nonusae/master/app/assets/shopify_asset/themes_asset/venture/collection-filters.liquid')
+      end  
   end
 
 
