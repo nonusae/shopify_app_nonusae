abca709ee9690b80eb963591658752f105f43331
Natthakit <nonusae@gmail.com>
2017-04-19 13:59:02 +0700

    theme automate

--- app/controllers/recurring_application_charges_controller.rb
+++ app/controllers/recurring_application_charges_controller.rb
@@ -5,6 +5,7 @@ class RecurringApplicationChargesController < AuthenticatedController
   def show
     @shop_domain=params[:shop]
     @charge_status = params[:status] if params[:status].present?
+    puts "STATUS STAUS " + params[:status]
     puts "STATUS IS" + @chrage_status if @charge_status.present?
     # if @shop_domain.present?
     #   redirect_to root_path(:shop => @shop_domain)
