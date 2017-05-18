b26ad3326de6519a88f1b18843b57dfad0556c8e
Natthakit <nonusae@gmail.com>
2017-05-18 17:51:51 +0700

    new update 2

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -5,6 +5,6 @@ ShopifyApp.configure do |config|
   config.embedded_app = true
   config.scripttags = [
   # {event:'onload', src: 'https://rawgit.com/nonusae/shopify_app_nonusae/master/app/assets/javascripts/test_script37.js'}
-    {event:'onload', src: 'https://cdn.rawgit.com/nonusae/shopify_app_nonusae/master/app/assets/javascripts/test_script39.js'}
+    {event:'onload', src: 'https://cdn.rawgit.com/nonusae/shopify_app_nonusae/cd32621a/app/assets/javascripts/test_script39.js'}
 ]
 end
