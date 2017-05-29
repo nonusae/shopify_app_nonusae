65ea279737be38d15217b084279e87299b0a5903
Natthakit <nonusae@gmail.com>
2017-05-29 13:21:01 +0700

    test email with gamil

--- app/mailers/main_app_mailer.rb
+++ app/mailers/main_app_mailer.rb
@@ -1,6 +1,6 @@
 class MainAppMailer < ApplicationMailer
 
-	default from: "Solutionsify"
+	default :from => "Solutionsify <no-reply@email.com>"
 
 	def sample_email(shop_domain,email,shop_name)
 		@shop_domain = shop_domain
