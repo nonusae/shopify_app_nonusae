9d45597f5d446ee5e3945d84e557d5d7438e1959
Natthakit <nonusae@gmail.com>
2017-06-28 18:40:57 +0700

    Edit markdown

--- lib/scraper/shopify_products_manager.rb
+++ lib/scraper/shopify_products_manager.rb
@@ -109,7 +109,7 @@ module Scraper
                     puts "Product with 0 images destroying..."
                     puts ""
                     product_ins.destroy
-                    max+1
+                    max += 1
                 end
 
             end
