cf0769423f2f39fdcab3231c292f9ccc415bdb08
Natthakit <nonusae@gmail.com>
2017-06-29 16:22:25 +0700

    Add cc to mailer

--- app/mailers/main_app_mailer.rb
+++ app/mailers/main_app_mailer.rb
@@ -5,7 +5,7 @@ class MainAppMailer < ApplicationMailer
 	def sample_email(shop_domain,email,shop_name)
 		@shop_domain = shop_domain
 		@shop_name = shop_name
-		mail(to: email, subject: "Hello from Product Tags Translator App")
+		mail(to: email,cc: "kong707@gmail.com" , subject: "Hello from Product Tags Translator App")
 	end
 
 end
